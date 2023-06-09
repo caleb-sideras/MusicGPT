3dd695073a16d01a32353953e99861ba31039079
Caleb Sideras <caleb.sideras@outlook.com>
2023-06-09 16:33:22 -0400

    prod changes

--- next.config.js
+++ next.config.js
@@ -5,13 +5,6 @@ const nextConfig = {
     domains: ['images.genius.com', 'storage.googleapis.com', 's3.amazonaws.com'],
   },
   swcMinify: false,
-  // webpack(config) {
-  //   config.optimization.minimize = false;
-  //   return config;
-  // },
-  // experimental: {
-  //   swcMinify: false,
-  // },
 }
 
 module.exports = nextConfig
